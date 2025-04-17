9d6f9e8d545b4c8bccc70e10504ad793a099e433
Dash Kwan <dashk@umich.edu>
2025-04-17 10:41:43 -0400

    new visualizations for dash

--- dash_visualizations.py
+++ dash_visualizations.py
@@ -30,22 +30,23 @@ avg_revenues_in_millions = [revenue / 1_000_000 for revenue in avg_revenues]
 # print(avg_revenues)
 # print(avg_popularities)
 
-fig, ax1 = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 
 ax1.set_xlabel('Year')
 ax1.set_ylabel('Average Revenue (in Millions)', color='tab:blue')
 ax1.plot(years, avg_revenues_in_millions, marker='o', linestyle='-', color='tab:blue', label='Average Revenue')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
+ax1.set_title('Average Movie Revnue Per Year')
 
-ax2 = ax1.twinx()
+ax2.set_xlabel('Year')
 ax2.set_ylabel('Average Popularity', color='tab:orange')
 ax2.plot(years, avg_popularities, marker='o', linestyle='-', color='tab:orange', label='Average Popularity')
 ax2.tick_params(axis='y', labelcolor='tab:orange')
+ax2.set_title('Average Song Popularity Per Year')
 
-plt.title('Average Movie Revenue and Song Popularity Per Year')
 fig.tight_layout()
 
-plt.savefig('average_revenue_and_popularity.png')
+plt.savefig('average_revenue_and_popularity_separate.png')
 
 plt.show()
 
